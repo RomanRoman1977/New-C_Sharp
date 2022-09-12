afec2c205789f9d03b1a5f39d5d5f4b1b69922b2
Roman <kormakoff.roma@gmail.com>
2022-09-12 16:54:27 +0300

    Кратно лт двум числам

---
+++ Task14/Program.cs
@@ -0,0 +1,15 @@
+﻿// 14. Напишите програииу которая принимает
+// на вход число и проверяет кратно ли оно 7 и 23
+// 14 - нет
+// 46 - нет
+// 161 - да
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Multiplicity(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+bool result = Multiplicity(number);
+Console.WriteLine(result ? "Да" :  "Нет");
