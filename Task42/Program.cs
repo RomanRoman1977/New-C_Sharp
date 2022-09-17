097d6396b92b873818181795c65c40d194041b68
Roman <kormakoff.roma@gmail.com>
2022-09-17 18:16:36 +0300

    Преобразование десятичного числа в двоичное

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -3,5 +3,22 @@
 // 45 - 101101
 // 3 - 11
 // 2 - 10
+// 13 - 1101
 
+Console.Write("Введите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
+int ChangeToBin(int a)
+{
+    int result = 0;
+    int mult = 1;
+    while (a > 0)
+    {
+        result += a % 2 * mult; //или result = result + a % 2 * mult
+        a /= 2; // или a = a /2
+        mult *= 10;
+    }
+    return result;
+}
+int res = ChangeToBin(num);
+Console.WriteLine(res);
