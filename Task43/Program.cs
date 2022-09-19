3176b4da9e3b3b8e597a1a6544e21ae9417cda00
Roman <kormakoff.roma@gmail.com>
2022-09-19 15:23:28 +0300

    Точка пересечения двух прямых

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -4,4 +4,29 @@
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Введите число: ");
+double[,] array = new double[2, 2];
+double[] point = new double[2];
+
+Console.Write("Введите значение k1: ");
+array[0, 0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b1: ");
+array[0, 1] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+array[1, 0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+array[1, 1] = Convert.ToInt32(Console.ReadLine());
+
+double[] IntersectionPoint(double[,] array)
+{
+    point[0] = (array[1, 1] - array[0, 1]) / (array[0, 0] - array[1, 0]);
+    point[1] = array[0, 0] * ((array[1, 1] - array[0, 1]) / (array[0, 0] - array[1, 0])) + array[0, 1];
+    
+    if (array[0, 0] == array[1, 0] && array[0, 1] == array[1, 1]) Console.WriteLine("Прямые совпадают");
+    else if (array[0, 0] == array[1, 0] && array[0, 1] != array[1, 1]) Console.WriteLine("Прямые параллельны");
+    else Console.WriteLine($"Точка пересечения прямых с координатами: x = {point[0]}, y = {point[1]}");
+    return point;
+}
+IntersectionPoint(array);
+
+
+
